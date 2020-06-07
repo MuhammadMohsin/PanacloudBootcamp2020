2baa4d25211232032be658296f03fefa13c6b036
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-06-07 22:52:48 +0500

    Add content for session 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World from all of us!</h1>      
+    </div>
+  );
+}
+
+export default App;
