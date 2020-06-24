2646645c2256f8f708a37c7bd537ced4bc2667bb
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-06-24 23:26:17 +0500

    Add content for useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect( () => {
+
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/muhammadmohsin/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+    }
+    getRepos();
+
+    // "https://api.github.com/users/muhammadmohsin/repos"
+    // fetch('https://jsonplaceholder.typicode.com/posts/1')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     console.log(json);
+    //     setData(json);
+    //   })
+  },[])
+
   return (
     <div className="App">
-      <h1>Hello World from all of us!</h1>      
+      <h1>You are seeing all repositories</h1>
+      <ul>
+        {repos.map((repoObj, ind) => {
+          return (<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
     </div>
   );
 }
