45691c2ba0fe5ad1d2ff95269e36092e427ee6eb
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-06-28 23:16:38 +0500

    Add content for material ui login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import './App.css';
+import NavBar from './NavBar'
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, TextField, Button, CircularProgress } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: 400,
+    margin: '0 auto',
+    marginTop: 50,
+    textAlign: 'center',
+    padding: 15
+  },
+  input: {
+    width: '95%',
+    marginBottom: 10
+  }
+}));
+
 
 function App() {
+
+  const classes = useStyles();
+
+
   return (
-    <div className="App">
-      <h1>Hello World from all of us!</h1>      
+    <div className={classes.root}>
+      <NavBar />
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField label="Enter Email" className={classes.input}/>
+          <TextField label="Enter Password" className={classes.input}/>
+          <Button variant="contained" color="primary" className={classes.input}>
+            Login
+          </Button>
+        </form>
+
+        <CircularProgress value={50} />
+      </Paper>
     </div>
   );
 }
