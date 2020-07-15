404d660b2ed00bab8dc362633c37baa2da26de2c
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-07-15 23:22:27 +0500

    Add content for session 14

---
+++ src/sample1.ts
@@ -0,0 +1,22 @@
+// console.log("Hello world from TS");
+
+// let a = 1;
+// a = true;
+// a = [1,2,3]
+
+// let std = {};
+// console.log(std.name)
+
+// function a(b: number) {
+//     return b / 2
+// }
+// a(2)
+// a("z");
+
+// let firstName: string = "Ali";
+// let lastName: string = "Ahmed";
+// let age: number = 20;
+// let isActiveStudent: boolean = true;
+// let courses: string[] = ["TS", "JS", "React"];
+
+// let fullName: number = firstName + age; 
---
+++ src/sample2.ts
@@ -0,0 +1,22 @@
+// const isActive: boolean = true; 
+
+document.write("Hello From TS");
+
+// function isEven(num: number){
+//     return num % 2 === 0;
+// }
+
+// console.log(isEven(10));
+// console.log(isEven("a"));
+
+// let a:any = 1;
+// a = true;
+// a = [1,2,3];
+// console.log(a)
+
+// let a: unknown = 30
+// let b = a === 123
+// // let c = a + 10
+// if (typeof a === 'number') {
+//     let d = a + 10
+// }
