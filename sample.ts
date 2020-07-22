90f64e87fbecc51bc7b7ed82cb99bb75419a07f8
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-07-22 23:26:49 +0500

    Add content for session 16

---
+++ sample.ts
@@ -0,0 +1,30 @@
+// let a: number = 1;
+
+// type Student = {
+//     name: string
+//     age: number
+//     courses: string[]
+// }
+
+// const std1: Student = {
+//     name: 'Ali',
+//     age: 20,
+//     courses: ['AI', 'JS', 'HTML']
+// }
+
+// const arr:number[] = [true, 1, "abc"]
+
+// const arr:[boolean, number, string] = [true, 1, "abc"];
+// const arr:[boolean, number, ...string[]] = [true, 1, "abc", "def" ,"a1"];
+
+// const availableCourses: readonly string[] = ["AI", "JS", "IOT"];
+// availableCourses[1] = "BCC"
+
+// enum PIAIC {
+//     CNC,
+//     IOT,
+//     BCC,
+//     AIC,
+// }
+
+// console.log(PIAIC.CNC);
