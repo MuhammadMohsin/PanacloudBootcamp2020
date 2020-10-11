d9ce6b011046c85b152ff24a56ff1204a625eb69
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-10-11 23:20:33 +0500

    Update content for faunadb add message

--- functions/add_message/add_message.js
+++ functions/add_message/add_message.js
@@ -1,10 +1,28 @@
+var faunadb = require('faunadb'),
+  q = faunadb.query;
+
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
+
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const messageBody = JSON.parse(event.body);
+    var adminClient = new faunadb.Client({ secret: 'fnAD3k5_3EACB6gCBsRr3R3wpEKT_uo1PpVFackm' });
+
+    const result = await adminClient.query(
+      q.Create(
+        q.Collection('messages'),
+        { data: { detail:  messageBody.message} },
+      )
+    )
+
+    // const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message:  result.ref.id}),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,55 @@
 import React from 'react';
+import { Formik } from 'formik';
 
-export default function Home () {
-    return <h1>Hello World</h1>
+export default function Home() {
+    return <div>
+        <h1>Anywhere in your app!</h1>
+        <Formik
+            initialValues={{ message: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.message) {
+                    errors.message = 'Required';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                console.log(values);
+                fetch(`/.netlify/functions/add_message`, {
+                    method: 'post',
+                    body: JSON.stringify(values)
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+
+                    });
+            }}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+            }) => (
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="message"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.message}
+                        />
+                        {errors.message && touched.message && errors.message}
+                        <button type="submit" disabled={isSubmitting}>
+                            Add Message
+                        </button>
+                    </form>
+                )}
+        </Formik>
+    </div>
 }
