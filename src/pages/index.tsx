b217b6bb1ab84c66a5897d37ef37f4ff79f9ac96
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-10-18 18:27:20 +0500

    Add basic todo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from "react"
+import { useQuery, useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 
 // This query is executed at run time by Apollo.
-const APOLLO_QUERY = gql`
+const GET_TODOS = gql`
 {
     todos {
         task,
@@ -11,14 +11,55 @@ const APOLLO_QUERY = gql`
     }
 }
 `;
+const ADD_TODO = gql`
+    mutation addTodo($task: String!){
+        addTodo(task: $task){
+            task
+        }
+    }
+`
 
 export default function Home() {
-  const { loading, error, data } = useQuery(APOLLO_QUERY);
+    const [todos, setTodos] = useState([{}]);
+    let inputText;
 
-  return (
-      <div>
-          {JSON.stringify(data)}
-      </div>
-  );
-    
+    const [addTodo] = useMutation(ADD_TODO);
+    const addTask = () => {
+        addTodo({
+            variables: {
+                task: inputText.value
+            },
+            refetchQueries: [{ query: GET_TODOS }]
+        })
+        inputText.value = "";
+    }
+
+    const { loading, error, data } = useQuery(GET_TODOS);
+
+    // useEffect(() => {
+    //     if (data && data.todos) {
+    //         setTodos(data.todos);
+    //     }
+    // }, [])
+
+    if (loading)
+        return <h2>Loading..</h2>
+
+    return (
+        <div>
+            <label>
+                Add Task <br />
+                <input type="text" ref={node => {
+                    inputText = node;
+                }} />
+            </label>
+            <button onClick={addTask}>Add Task</button>
+
+            <br /> <br />
+
+            {JSON.stringify(data.todos)}
+
+        </div>
+    );
+
 }
