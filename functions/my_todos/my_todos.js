f58b59e3feb5fb1c01a24e20adcba09ddaf2c777
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-10-18 22:10:53 +0500

    add fauna db

--- functions/my_todos/my_todos.js
+++ functions/my_todos/my_todos.js
@@ -1,28 +1,89 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
+var faunadb = require('faunadb'),
+  q = faunadb.query;
 
 const typeDefs = gql`
   type Query {
     todos: [Todo!]
   }
+  type Mutation {
+    addTodo(task: String!): Todo
+  }
   type Todo {
     id: ID!
     task: String!
-    status: String!
+    status: Boolean!
   }
 `
 
-const todos = [
-  { id: 1, task: 'Terry Pratchett', status:"New" },
-  { id: 2, task: 'Stephen King', status:"New"},
-  { id: 3, task: 'JK Rowling', status:"New" },
+const todosList = [
+  { id: 1, task: 'My task 1', status: false },
+  { id: 2, task: 'My second task', status: true },
+  { id: 3, task: 'My third task', status: false },
 ]
 
 const resolvers = {
   Query: {
-    todos: (root, args, context) => {
-      return todos
+    todos: async (root, args, context) => {
+      // return todosList
+      try {
+        var adminClient = new faunadb.Client({ secret: 'fnAD3k5_3EACB6gCBsRr3R3wpEKT_uo1PpVFackm' });
+
+        const result = await adminClient.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index('task'))),
+            q.Lambda(x => q.Get(x))
+          )
+        );
+        // console.log(result.data)
+        // return todosList
+        return result.data.map(d => {
+          return {
+            id: d.ts,
+            task: d.data.task,
+            status: d.data.status
+          }
+        })
+      }
+      catch (err) {
+        console.log(err)
+      }
     }
   },
+  Mutation: {
+    addTodo: async (_, { task }) => {
+
+      /*const newTask = {
+        id: todosList.length + 1,
+        task,
+        status: false
+      }
+      todosList.push(newTask);
+      console.log(todosList);*/
+
+      try {
+        var adminClient = new faunadb.Client({ secret: 'fnAD3k5_3EACB6gCBsRr3R3wpEKT_uo1PpVFackm' });
+
+        const result = await adminClient.query(
+          q.Create(
+            q.Collection('todos'),
+            {
+              data: {
+                task: task,
+                status: false
+              }
+            },
+          )
+        );
+
+        console.log(result.data)
+        return (result.data)
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+  }
 }
 
 const server = new ApolloServer({
